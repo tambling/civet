f1d52f072ebebda626085eca58e675af08bf2757
Dean Tambling <dean.tambling@gmail.com>
2015-11-09 09:55:27 -0800

    Nearly initial commit.

---
+++ civet.js
@@ -0,0 +1,54 @@
+function Civet(){
+  this.data = {};
+  this.id = 1;
+};
+
+Civet.prototype.create = function(entity, callback){
+  var error = null;
+
+  if((entity instanceof Object) && !Array.isArray(entity)) {
+    entity.id = this.id;
+    this.data[entity.id] = Object.assign({}, entity);
+    this.id += 1; 
+  } else {
+    error = "You need to pass in an object!";
+  }
+
+  callback(error);
+}
+
+Civet.prototype.find = function(callback){
+  var error = null;
+  
+  if(Object.keys(this.data).length === 0){
+    error = "No records found!";
+  }
+
+  var entities = [];
+  for (entity in this.data) {
+    if(this.data.hasOwnProperty(entity)){
+      entities.push(this.data[entity])
+    }
+  }
+  callback(error, entities);
+}
+
+Civet.prototype.findById = function(id, callback) {
+  var entity = this.data[id];
+  var error = entity ? null : "No record found!";
+
+  callback(error, entity);
+}
+
+Civet.prototype.remove = function(id, callback){
+  var entity = Object.assign({}, this.data[id]);
+  var error = null;
+
+  if(entity){
+    delete this.data[id]
+  } else {
+    error = "No record found!";
+  }
+
+  callback(error, entity);
+}
